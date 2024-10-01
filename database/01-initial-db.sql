5b501071e6bb69a04f2015179fbe6777ad6e0b57
swaleh <ethereumdevelopment@gmail.com>
2024-10-01 14:29:18 +0100

    Set up MySQL database and seeded initial data

--- database/01-initial-db.sql
+++ database/01-initial-db.sql
@@ -1,8 +1,15 @@
-use definitions;
+USE definitions;
 
 CREATE TABLE IF NOT EXISTS `dashboards` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `createdAt` TIMESTAMP NOT NULL DEFAULT NOW(),
   `updatedAt` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+  `title` VARCHAR(255) NOT NULL, 
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Insert sample data
+INSERT INTO dashboards (createdAt, updatedAt, title) VALUES
+(NOW(), NOW(), 'Dashboard 1'),
+(NOW(), NOW(), 'Dashboard 2'),
+(NOW(), NOW(), 'Dashboard 3');
